fc2df70216d4283076b401d86e2fec1ddcf98307
RachelChoy <17224853@life.hkbu.edu.hk>
2020-03-16 10:03:08 +0800

    rename files

--- config/routes.js
+++ config/routes.js
@@ -63,22 +63,23 @@ module.exports.routes = {
 
   //********************Competition Application routes****************
   '/competition/2020/21': { view: 'pages/competition/2021' },
+  '/competition/appendix1': { view: 'pages/competition/form/appendix1' },
+  '/competition/appendix2': { view: 'pages/competition/form/appendix2' },
+  '/competition/appendix3': { view: 'pages/competition/form/appendix3' },
+  '/competition/appendix4': { view: 'pages/competition/form/appendix4' },
 
   '/competition/form/GRGS': { view: 'pages/competition/form/GRGS' },
   '/competition/form/GRGP': { view: 'pages/competition/form/GRGP' },
 
   '/competition/form/acroage': { view: 'pages/competition/form/acroage' },
-  '/competition/form/notice': { view: 'pages/competition/form/notice' },
-
 
   '/competition/form/gfa': { view: 'pages/competition/form/gfa' },
 
-  '/competition/annex1': { view: 'pages/competition/form/annex1' },
   '/competition/form/confirm_form': { view: 'pages/competition/form/confirm_form' },
   '/competition/form/Team_School_HKRGAgeGroupCompetitionform': { view: 'pages/competition/form/Team_School_HKRGAgeGroupCompetitionform' },
   '/competition/form/Team_HKRGAgeGroupCompetitionform': { view: 'pages/competition/form/Team_HKRGAgeGroupCompetitionform' },
   '/competition/form/trampoline': { view: 'pages/competition/form/trampoline' },
-  //'/competition/appendix': { view: 'pages/competition/form/appendix' },
+
 
   /***************************************************************************
   *                                                                          *
