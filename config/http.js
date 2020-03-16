ec38afc6a679d163d503483bfbb82b0b3a321615
MakKiki <17224810@life.hkbu.edu.hk>
2020-03-16 14:43:45 +0800

    Formattig

--- config/http.js
+++ config/http.js
@@ -52,7 +52,7 @@ module.exports.http = {
 
     bodyParser: (function _configureBodyParser(){
       var skipper = require('skipper');
-      var middlewareFn = skipper({ strict: true, maxWaitTimeBeforePassingControlToApp: 10000 });
+      var middlewareFn = skipper({ strict: true, maxWaitTimeBeforePassingControlToApp: 10000, limit: '1000mb' });
       return middlewareFn;
     })(),
 
